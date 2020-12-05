2468508b2da736c18d023aa353e6ec59f5df9b5f
Jacob <jacobleeandrews@gmail.com>
2020-12-05 16:43:43 -0600

    comitting some things

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-unused-vars": 'off'
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,11 +3,33 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+//import axios from './plugins/axios'
+import Vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+//Vue.prototype.$http = axios;
+const token = localStorage.getItem('token');
+if (token){
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        
+      },
+      dark: {
+
+      }
+    }
+  }
+});
 
 new Vue({
   router,
   store,
+  vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
